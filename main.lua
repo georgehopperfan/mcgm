bf4920574be9e2d572390620f7eb99930144fa50
georgehopperfan <georgehopperfan67@gmail.com>
2026-04-19 16:03:30 +0800

    Add files via upload

--- main.lua
+++ main.lua
@@ -6,6 +6,14 @@ SMODS.Atlas({
     atlas_table = "ASSET_ATLAS"
 })
 
+SMODS.Atlas({
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+}):register()
+
 local NFS = require("nativefs")
 to_big = to_big or function(a) return a end
 lenient_bignum = lenient_bignum or function(a) return a end
